512295f11419caa686b1dae6d872c1a8f8a51a6c
hyperlogin <hyper_login@hotmail.com>
2017-12-20 16:52:47 +0800

    Update maps.js

--- maps.js
+++ maps.js
@@ -6,19 +6,36 @@ function initialize() {
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
   var map = new google.maps.Map(document.getElementById("map"), mapProp);
-  var infowindow = new google.maps.InfoWindow({
+  var infowindowBK = new google.maps.InfoWindow({
           content: "MC Concept Mobile"
         });
+  var infowindowGL = new google.maps.InfoWindow({
+        content:"Another info here"
+  });
   var marker = new google.maps.Marker({
     position: myCenter,
     animation: google.maps.Animation.DROP
   });
+  
+  var marker2 = new google.maps.Market({
+    position: {1.3104433,103.8750213},
+    animation:google.maps.Animation.DROP
+  });
+  
   marker.addListener('click', function()
   {
-    infowindow.open(map, marker);
+    infowindowBK.open(map, marker);
     toggleBounce();
   });
+  
+  marker2.addListener('click', function()
+  {
+    infowindowGL.open(map, marker2);
+    toggleBounce();
+  });
+  
   marker.setMap(map);
+  marker2.setMap(map);
 }
 function toggleBounce() {
         if (marker.getAnimation() !== null) {
