6946a37c3a3b22315642c2b351fb6a0dfe7b65d7
hyperlogin <hyper_login@hotmail.com>
2017-12-20 16:59:23 +0800

    Update maps.js

--- maps.js
+++ maps.js
@@ -6,36 +6,50 @@ function initialize() {
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
   var map = new google.maps.Map(document.getElementById("map"), mapProp);
-  var infowindowBK = new google.maps.InfoWindow({
-          content: "MC Concept Mobile"
+  var infoWindowContent = [
+        ['<div class="info_content">' +
+        '<h3>Boon Keng Branch</h3>' +
+        '<p>The London Eye is a giant Ferris wheel situated on the banks of the River Thames. The entire structure is 135 metres (443 ft) tall and the wheel has a diameter of 120 metres (394 ft).</p>' +        '</div>'],
+        ['<div class="info_content">' +
+        '<h3>Another Branch</h3>' +
+        '<p>The Palace of Westminster is the meeting place of the House of Commons and the House of Lords, the two houses of the Parliament of the United Kingdom. Commonly known as the Houses of Parliament after its tenants.</p>' +
+        '</div>']
+    ];
+  var markers = [
+        ['London Eye, London', 1.319135,103.8629299],
+        ['Palace of Westminster, London', 1.3104433,103.8750213]
+   ];
+  
+  // Display multiple markers on a map
+    var infoWindow = new google.maps.InfoWindow(), marker, i;
+    
+    // Loop through our array of markers & place each one on the map  
+    for( i = 0; i < markers.length; i++ ) {
+        var position = new google.maps.LatLng(markers[i][1], markers[i][2]);
+        bounds.extend(position);
+        marker = new google.maps.Marker({
+            position: position,
+            map: map,
+            title: markers[i][0]
         });
-  var infowindowGL = new google.maps.InfoWindow({
-        content:"Another info here"
-  });
-  var marker = new google.maps.Marker({
-    position: myCenter,
-    animation: google.maps.Animation.DROP
-  });
-  
-  var marker2 = new google.maps.Market({
-    position: {lat:1.3104433,lng:103.8750213},
-    animation:google.maps.Animation.DROP
-  });
-  
-  marker.addListener('click', function()
-  {
-    infowindowBK.open(map, marker);
-    toggleBounce();
-  });
-  
-  marker2.addListener('click', function()
-  {
-    infowindowGL.open(map, marker2);
-    toggleBounce();
-  });
-  
-  marker.setMap(map);
-  marker2.setMap(map);
+        
+        // Allow each marker to have an info window    
+        google.maps.event.addListener(marker, 'click', (function(marker, i) {
+            return function() {
+                infoWindow.setContent(infoWindowContent[i][0]);
+                infoWindow.open(map, marker);
+            }
+        })(marker, i));
+
+        // Automatically center the map fitting all markers on the screen
+        map.fitBounds(bounds);
+    }
+
+    // Override our map zoom level once our fitBounds function runs (Make sure it only runs once)
+    var boundsListener = google.maps.event.addListener((map), 'bounds_changed', function(event) {
+        this.setZoom(14);
+        google.maps.event.removeListener(boundsListener);
+    });
 }
 function toggleBounce() {
         if (marker.getAnimation() !== null) {
